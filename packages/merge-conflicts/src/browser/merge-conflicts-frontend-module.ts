d4183fd32964dfd7bc1f55710d70b21593051922
Alex Tugarev <alex.tugarev@typefox.io>
2018-02-01 11:38:19 +0100

    add scaffold for merge-conflicts extension

---
+++ packages/merge-conflicts/src/browser/merge-conflicts-frontend-module.ts
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2018 TypeFox and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License'); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+import { ContainerModule } from 'inversify';
+// import { CommandContribution, MenuContribution, bindContributionProvider } from '@theia/core/lib/common';
+// import { OpenHandler, WidgetFactory, FrontendApplicationContribution } from '@theia/core/lib/browser';
+
+import '../../src/browser/style/index.css';
+
+export default new ContainerModule(bind => {
+});
